33c41f62e5ee74e5d7814a68813540e56a1848f8
Baptiste <baptiste.franel@gmail.com>
2024-10-01 19:29:36 +0200

    switched to used SHA

--- LoginServer/Core/Handling/LoginPacketHandler.LoginClient.cs
+++ LoginServer/Core/Handling/LoginPacketHandler.LoginClient.cs
@@ -1,7 +1,6 @@
 ﻿using RevolutionCore.Networking;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,9 +17,76 @@ namespace RoseLoginServer.Core.Handling
         /// <param name="client">Client.</param>
         /// <param name="packet">Packet.</param>
         /// <returns>Task.</returns>
-        public async Task TestReply(LoginClient client, Packet packet)
+        public async Task HandleLogin(LoginClient client, Packet packet)
         {
-            await SendPacket(TestPacket(210), client);
+            ushort size = packet.Size;
+            ushort command = packet.Command;
+            byte[] dataBytes = packet.GetBytes(size - 4);
+
+            string dataString = Encoding.UTF8.GetString(dataBytes);
+
+            string[] credentials = dataString.Split(':');
+
+            if (credentials.Length == 2)
+            {
+                string username = credentials[0].Replace("\0", string.Empty).Trim();
+                string password = credentials[1].Replace("\0", string.Empty).Trim();
+
+                Cryptography passwordManager = new Cryptography();
+                dbAccountService accountService = new dbAccountService(database);
+                var user = await accountService.GetUserAsync(username);
+
+                Console.WriteLine($"Login : {user.Username}, Password : {user.Password}, Salt : {user.Salt}");
+
+                if (user == null)
+                {
+                    await SendPacket(LoginFailedPacket("Something went wrong, could not login."), client);
+                }
+
+                if (user != null)
+                {
+                    bool loginSuccess = passwordManager.VerifyPassword(password, user.Password, user.Salt);
+
+                    if (loginSuccess)
+                    {
+                        await SendPacket(LoginSuccessfullPacket(), client);
+                    }
+                    else
+                    {
+                        await SendPacket(LoginFailedPacket("Wrong password."), client);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid chain format, ':' is missing.");
+            }
+        }
+
+    }
+
+    public class Cryptography
+    {
+        public string HashPassword(string password, string salt)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                string saltedPassword = password + salt; // Ajouter le sel au mot de passe
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(saltedPassword));
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2")); // Convertir en hexadécimal
+                }
+                return builder.ToString();
+            }
+        }
+
+        public bool VerifyPassword(string password, string hashedPassword , string salt)
+        {
+            string hashedInputPassword = HashPassword(password, salt);
+            Console.WriteLine($"hashedInputPassword : {hashedInputPassword}, hashed : {hashedPassword}");
+            return hashedPassword.Equals(hashedInputPassword, StringComparison.OrdinalIgnoreCase);
         }
     }
 }
--- RevolutionCore/SQL/Database.cs
+++ RevolutionCore/SQL/Database.cs
@@ -1,10 +1,10 @@
 ﻿using RevolutionCore.Utils;
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
+using Npgsql;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace RevolutionCore.SQL
 {
@@ -14,7 +14,7 @@ namespace RevolutionCore.SQL
     public partial class Database
     {
         readonly string connectionString;
-        readonly SqlConnection connection;
+        readonly NpgsqlConnection connection;
 
         /// <summary>
         /// Constructor.
@@ -22,11 +22,12 @@ namespace RevolutionCore.SQL
         /// <param name="database">Database name.</param>
         /// <param name="user">User.</param>
         /// <param name="password">Password.</param>
-        public Database(string database, string user, string password)
+        public Database(string dbip, string port, string database, string user, string password)
         {
-            connectionString = $@"Data Source =.; Initial Catalog = {database}; User ID = {user}; Password = {password};";
+            Logger.LogImportantMessage("Db infos :", $"{dbip}, {port}, {database}, {user}, {password}");
+            string connectionString = $"Host={dbip};Port={port};Database={database};Username={user};Password={password};SSL Mode=Prefer;Trust Server Certificate=True;";
 
-            connection = new SqlConnection(connectionString);
+            connection = new NpgsqlConnection(connectionString);
         }
 
         /// <summary>
@@ -48,7 +49,60 @@ namespace RevolutionCore.SQL
             {
                 Logger.LogFatalError($"Can't open database on SQL server : {ex.Message}");
 
+
                 return false;
+            }
+        }
+
+        /// <summary>
+        /// Close the database connection.
+        /// </summary>
+        public void Close()
+        {
+            if (connection != null && connection.State != System.Data.ConnectionState.Closed)
+            {
+                connection.Close();
+            }
+        }
+
+        /// <summary>
+        /// Returns objects found in db.
+        /// </summary>
+        /// <typeparam name="T">The type of the result object.</typeparam>
+        /// <param name="query">SQL query.</param>
+        /// <param name="parameters">Query parameters (optional).</param>
+        /// <returns>List of T (the result set).</returns>
+        public async Task<IEnumerable<T>> QueryAsync<T>(string query, object parameters = null)
+        {
+            try
+            {
+                return await connection.QueryAsync<T>(query, parameters); // Utilise la version asynchrone de Dapper
+            }
+            catch (Exception ex)
+            {
+                Logger.LogFatalError($"QueryAsync failed: {ex.Message}");
+                return Enumerable.Empty<T>(); // Retourne une liste vide si une erreur se produit
+            }
+        }
+
+        /// <summary>
+        /// Returns first object found in db
+        /// </summary>
+        /// <typeparam name="T">The type of the result object.</typeparam>
+        /// <param name="query">SQL query.</param>
+        /// <param name="parameters">Query parameters (optional).</param>
+        /// <returns>List of T (the result set).</returns>
+        public async Task<T> QuerySingleOrDefaultAsync<T>(string query, object parameters = null)
+        {
+            try
+            {
+                var result = await connection.QueryAsync<T>(query, parameters);
+                return result.FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogFatalError($"QuerySingleOrDefaultAsync failed: {ex.Message}");
+                return default;
             }
         }
     }
