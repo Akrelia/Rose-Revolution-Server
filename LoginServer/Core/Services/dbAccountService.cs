bf8b465faba13df58cc78489d92609b37ebc4347
Baptiste <baptiste.franel@gmail.com>
2024-10-01 18:14:52 +0200

    added login logic

---
+++ LoginServer/Core/Services/dbAccountService.cs
@@ -0,0 +1,30 @@
+﻿using RevolutionCore.SQL;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class dbAccountService
+{
+    private readonly Database _database;
+
+    public dbAccountService(Database database)
+    {
+        _database = database;
+    }
+
+    public async Task<User> GetUserAsync(string username)
+    {
+        Console.WriteLine(username.Length);
+        string query = "SELECT username, password FROM accounts WHERE username = @Username";
+
+        return await _database.QuerySingleOrDefaultAsync<User>(query, new { Username = username });
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
+
---
+++ RevolutionCore/Utils/PasswordManager.cs
@@ -0,0 +1,18 @@
+﻿using BCrypt.Net;
+
+public class PasswordManager
+{
+    // Hasher un mot de passe
+    public string HashPassword(string password)
+    {
+        int saltRounds = 10;
+        string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password, saltRounds);
+        return hashedPassword;
+    }
+
+    // Vérifier un mot de passe avec son hash
+    public bool VerifyPassword(string password, string hash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hash);
+    }
+}
